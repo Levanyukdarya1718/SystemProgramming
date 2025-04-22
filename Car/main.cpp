4f43bd3f59f9cde8369e4be798e1f4be2e9064b9
User <User@DESKTOP-B06BQ3L>
2025-04-22 20:48:51 +0500

    EngineHomeworkDONE

--- Car/main.cpp
+++ Car/main.cpp
@@ -24,7 +24,6 @@ public:
 		//this->CAPACITY = capacity;
 		this->fuel_level = 0;
 		cout << "Tank is ready" << endl;
-
 	}
 	~Tank()
 	{
@@ -55,12 +54,59 @@ public:
 		cout << "Fuel level:\t" << fuel_level << "liters.\n";
 	}
 };
+#define MIN_ENGINE_CONSUMPTION	5
+#define MAX_ENGINE_CONSUMPTION	50
+class Engine
+{
+	double consumption_ls;
+	bool running;
+public:
+	const double CONSUMPTION;
+
+	double get_consuption_ls()const
+	{
+		return consumption_ls;
+	}
+	Engine(double consumption) :CONSUMPTION
+	(
+
+		consumption < MIN_ENGINE_CONSUMPTION ? MIN_ENGINE_CONSUMPTION :
+		consumption > MAX_ENGINE_CONSUMPTION ? MAX_ENGINE_CONSUMPTION :
+		consumption
+
+	), running(false)
+	{
+		
+		consumption_ls = CONSUMPTION * 0.0003;
+		cout << "Engine is ready:" << endl;
+	}
+	~Engine()
+	{
+		cout << "Engine is over"  << endl;
+	}
+	void engine_start()
+	{
+		running = true;
+	}
+	void engine_stop()
+	{
+		running = false;
+	}
+	void info()const
+	{
+		cout << CONSUMPTION << " l/100km" << endl;
+		cout << consumption_ls << " l/s" << endl;
+	}
+
+};
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	Tank tank(80);
 	tank.info();
+	Engine engine(10);
+	engine.info();
 	double fuel;
 	do
 	{
@@ -68,5 +114,6 @@ void main()
 		tank.fill(fuel);
 		tank.info();
 	} while (true);
+
 
 }
